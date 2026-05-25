3d276ada801b4c11d27c34507ecd31a4b764e647
Hagit Eizen <hagithalle@gmail.com>
2026-05-25 23:30:14 +0300

    fix: update DailyHabitsList.tsx import and UI tweaks

--- client/src/components/habits/DailyHabitsList.tsx
+++ client/src/components/habits/DailyHabitsList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { habitsApi, HabitCompletion } from '../api/habitsApi';
+import { habitsApi, HabitCompletion } from '../../api/habitsApi';
 import { Box, Checkbox, Typography, CircularProgress, Alert } from '@mui/material';
 
 export default function DailyHabitsList() {
